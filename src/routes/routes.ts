8edcd2358e70340d7561da3a71ef8cc05cde27a6
dasari234 <dasari9@gmail.com>
2019-04-12 16:53:13 +0530

    route changes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,6 @@ import * as express from 'express';
 export const routes = express.Router();
 
 routes.get('', (req, res) => res.send({title: 'world', name:'Srinivas Dasari'}));
-routes.get('/api', (req, res) => res.send({hello: 'world'}));
-routes.get('/api/users', (req, res) => res.send({hello: 'world...'}));
+routes.get('/api', (req, res) => res.send({hello: 'world ../ api'}));
+routes.get('/api/users', (req, res) => res.send({hello: 'world.../users'}));
 routes.post('/users', (req, res) => res.send({hello: 'welcome************.'}));
